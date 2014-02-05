60d638e975668004fab8cff44e67c9ca53b62e3a
Moim Hossain <moimhossain@gmail.com>
2014-02-05 14:47:59 +0100

    Update Program.cs
    added some comments

--- WcfClient/Program.cs
+++ WcfClient/Program.cs
@@ -1,4 +1,8 @@
-﻿
+
+
+
+// No License or anything. Feel free to use it anyway you like. 
+// Mentioning the origonal authors name would be greatly appreciated but not necessary at all.﻿
 
 using Contracts;
 using System;
