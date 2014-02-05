2aa6e5a58e6b17214feb3c49aa9c10cbaa966ba1
Moim Hossain <moimhossain@gmail.com>
2014-02-05 14:48:17 +0100

    Update Program.cs
    added some comments

--- WcfServer/Program.cs
+++ WcfServer/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 
+// No License or anything. Feel free to use it anyway you like. 
+// Mentioning the origonal authors name would be greatly appreciated but not necessary at all.
 
 using Contracts;
 using System;
